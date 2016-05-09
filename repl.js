7254c10f09ef8707dec53f20867bb7843e44e05a
Nikhil Ranjan <nikhil.ranjan@walmart.com>
2016-05-09 13:50:37 +0530

    Node js REPL

---
+++ repl.js
@@ -0,0 +1,27 @@
+const vm = require('vm');
+
+process.stdin.resume();
+
+let exit = false;
+
+const cursor = () => process.stdout.write("> ");
+
+cursor();
+process.stdin.on("data", (data) => {
+	exit = false;
+	if (data.toString().trim() === ".exit") process.exit();
+	process.stdout.write(JSON.stringify(vm.runInThisContext(data.toString()), null, 2) + "\n");
+	cursor();
+});
+
+process.on('SIGINT', function() {
+	if (exit) process.exit();
+    process.stdout.write(`\n(To exit, press ^C again or type .exit)\n`);
+    exit = true;
+    cursor();
+});
+
+process.on("uncaughtException", (e)=>{
+	console.log(e.stack);
+	cursor();
+});
